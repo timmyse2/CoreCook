a292a10a65cd4ffec82c46ba19fd9d56daf5032f
timmy <timmyse2@yahoo.com.tw>
2023-12-27 10:46:00 +0800

    Java Script sample for ch9
    with ignore list;

---
+++ Core_CodeFirst/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_CodeFirst.Models
+{
+    public class Location
+    {
+        public string City { get; set; }
+        public double[] Temp { get; set; }
+
+        //public int[] GetNumber()
+        //{
+        //    return
+        //}
+    }
+}
